de970137123caea10456ba99d7eabb871a48e9a0
Damon Greenhalgh <damonligreenhalgh@gmail.com>
2022-06-08 15:36:07 +1200

    Add documentation and variable changes

--- CS373LicensePlateDetection.py
+++ CS373LicensePlateDetection.py
@@ -1,3 +1,9 @@
+'''
+COMPSCI 373 Assignment - License Plate Detection
+Name: Damon Greenhalgh
+UPI: dgre615
+'''
+
 import math
 import sys
 from pathlib import Path
@@ -72,107 +78,54 @@ def createInitializedGreyscalePixelArray(image_width, image_height, initValue = 
     new_array = [[initValue for x in range(image_width)] for y in range(image_height)]
     return new_array
 
-# This function computes the standard deviation of the input image.
-def standardDeviation(pixel_array, image_width, image_height):
+
+def convertToGreyscale(image_width, image_height, px_array_r, px_array_g, px_array_b):
+    '''
+    This function converts an RGB image to GREYSCALE image.
+    '''
+
+    image = createInitializedGreyscalePixelArray(image_width, image_height)
+
+    for i in range(image_height):
+        for j in range(image_width):
+            # Each color has specific coefficients based on their natural 'intensity'.
+            image[i][j] = 0.299 * px_array_r[i][j] + 0.587 * px_array_g[i][j] + 0.114 * px_array_b[i][j]
+
+    return image
+
+
+def computeStandardDeviation(pixel_array, image_width, image_height):
+    '''
+    This function computes a 5x5 Standard Deviation filter across the argument
+    pixel_array to find high contrast structures within the image.
+    '''
+
     image = createInitializedGreyscalePixelArray(image_width, image_height, 0.0)
-    region_size = 2
-    region = [(i - region_size) for i in range(region_size * 2 + 1)]
-    norm = pow(region_size * 2 + 1, 2)
-    print(region, norm)
-    for i in range(region_size, image_height - region_size):
-        for j in range(region_size, image_width - region_size):
+
+    for i in range(2, image_height - 2):
+        for j in range(2, image_width - 2):
 
             values = []
 
-            for k in region:
-                for l in region:
+            for k in [-2, -1, 0, 1, 2]:
+                for l in [-2, -1, 0, 1, 2]:
+
+                    # Add all pixels in a 5x5 neighbourhood
                     values.append(pixel_array[i + k][j + l])
 
-            mean = sum(values) / norm
-            image[i][j] = pow(sum([pow(value - mean, 2) for value in values]) / norm, 0.5)
+            # Compute mean and standard deviation
+            mean = sum(values) / 25
+            sd = pow(sum([pow(value - mean, 2) for value in values]) / 25, 0.5)
+            image[i][j] = sd
 
     return image
 
 
-# Helper function to compute the min and max values of a image
-def computeMinAndMaxValues(pixel_array, image_width, image_height):
-    
-    # Default min and max values. At the boundary.
-    max_value = 0
-    min_value = 255
-    
-    # Iterate over every pixel in the image
-    for i in range(image_height):
-        for j in range(image_width):
-            
-            value = pixel_array[i][j]
-            
-            # Update max
-            if value > max_value:
-                max_value = value
-            
-            # Update min
-            if value < min_value:
-                min_value = value
-                
-    return (min_value, max_value)
-
-            
-# This function scales the intensities of an image
-def scale(pixel_array, image_width, image_height):
-    
-    # Create identical dimension blank image to be used as a updated version of 
-    # the parameter image after transformation.
-    image = createInitializedGreyscalePixelArray(image_width, image_height)
-    
-    # Find min and max values of the image with the use of a helper function below
-    f_min, f_max = computeMinAndMaxValues(pixel_array, image_width, image_height)
-
-    # Default bounds
-    g_min, g_max = 0, 255
-    
-    # In the case min and max of the image are the same return blank image
-    if f_min == f_max:
-        return image
-    
-    # Iterate over every pixel
-    for i in range(image_height):
-        for j in range(image_width):
-            
-            # Compute gain and bias coefficients
-            a = (g_max - g_min)/(f_max - f_min)
-            b = g_min - f_min*a
-            
-            # Generate transformed value
-            new_value = round(a*pixel_array[i][j] + b)
-            
-            # Clamp to keep new_value between the bounds
-            # Keep new_value <= g_max (255)
-            if new_value > g_max:
-                new_value = g_max
-            
-            # Keep new_value >= g_min (0)
-            if new_value < g_min:
-                new_value = g_min
-            
-            # Update pixel in image
-            image[i][j] = new_value
-    
-    return image
-
-
-# This function converts a rgb image to a greyscale image
-def convertToGreyscale(image_width, image_height, px_array_r, px_array_g, px_array_b):
-
-    greyscale_image = createInitializedGreyscalePixelArray(image_width, image_height)
-
-    for i in range(image_height):
-        for j in range(image_width):
-            greyscale_image[i][j] = 0.299*px_array_r[i][j] + 0.587*px_array_g[i][j] + 0.114*px_array_b[i][j]
-    
-    return greyscale_image
-
-def thresholdSegmentation(pixel_array, image_width, image_height, threshold):
+def computeThresholdSegmentation(pixel_array, image_width, image_height, threshold):
+    '''
+    This function converts an image to a binary black and white image. Pixel values
+    are determined by the threshold argument.
+    '''
 
     image = createInitializedGreyscalePixelArray(image_width, image_height)
 
@@ -187,33 +140,110 @@ def thresholdSegmentation(pixel_array, image_width, image_height, threshold):
     return image
 
 
-def dilation(pixel_array, image_width, image_height):
+def computeMinAndMaxValues(pixel_array, image_width, image_height):
+    '''
+    Helper function for the scale() function. This functions finds the min
+    and max values of the pixel_array argument.
+    '''
+
+    # Default min and max values, at the boundary
+    max_value = 0
+    min_value = 255
+
+    # Iterate over every pixel in the image
+    for i in range(image_height):
+        for j in range(image_width):
+
+            value = pixel_array[i][j]
+
+            # Update max if pixel value is larger then current max_value
+            if value > max_value:
+                max_value = value
+
+            # Update min if pixel value is smaller then current min_value
+            if value < min_value:
+                min_value = value
+
+    return (min_value, max_value)
+
+
+def computeScale(pixel_array, image_width, image_height):
+    '''
+    This function scales the intensities of an image to its max range (0, 255).
+    '''
+
+    # Create new blank image that we will update and return
     image = createInitializedGreyscalePixelArray(image_width, image_height)
 
+    # Find min and max values of pixel_array
+    f_min, f_max = computeMinAndMaxValues(pixel_array, image_width, image_height)
+
+    # Default bounds
+    g_min, g_max = 0, 255
+
+    # In the case min and max of the image are the same return blank image
+    if f_min == f_max:
+        return image
+
+    # Iterate over every pixel
+    for i in range(image_height):
+        for j in range(image_width):
+
+            # Compute gain and bias coefficients
+            a = (g_max - g_min) / (f_max - f_min)
+            b = g_min - f_min * a
+
+            # Generate transformed value
+            new_value = round(a * pixel_array[i][j] + b)
+
+            # Clamp to keep new_value between the bounds
+            # Keep new_value <= g_max (255)
+            if new_value > g_max:
+                new_value = g_max
+
+            # Keep new_value >= g_min (0)
+            if new_value < g_min:
+                new_value = g_min
+
+            # Update pixel in image
+            image[i][j] = new_value
+
+    return image
+
+
+def computeDilation(pixel_array, image_width, image_height):
+    '''
+    This function performs a dilation operation on the argument pixel_array.
+    '''
+
+    image = createInitializedGreyscalePixelArray(image_width, image_height)
+
+    # Structuring element
     se = [
         [1, 1, 1],
         [1, 1, 1],
         [1, 1, 1]
     ]
 
+    # For each pixel in the pixel_array
     for i in range(image_height):
         for j in range(image_width):
 
+            # Hit flag, assume false and flag true if at least
+            # one pixel in se matches.
             hit = False
 
+            # Iterate over a 3x3 neighbourhood
             for k in [-1, 0, 1]:
                 for l in [-1, 0, 1]:
 
                     y = i + k
                     x = j + l
 
+                    # Keep index within the bounds of the image.
                     if -1 < y < image_height and -1 < x < image_width:
 
-                        active = False
-                        if pixel_array[y][x] == 1 or pixel_array[y][x]:
-                            active = True
-
-                        if active and 1 == se[k + 1][l + 1]:
+                        if pixel_array[y][x] != 0 and se[k + 1][l + 1] == 1:
                             hit = True
 
             if hit:
@@ -222,10 +252,14 @@ def dilation(pixel_array, image_width, image_height):
     return image
 
 
-def erosion(pixel_array, image_width, image_height):
+def computeErosion(pixel_array, image_width, image_height):
+    '''
+    This function performs a erosion operation on the argument pixel_array.
+    '''
 
     image = createInitializedGreyscalePixelArray(image_width, image_height)
 
+    # Structuring element
     se = [
         [1, 1, 1],
         [1, 1, 1],
@@ -235,6 +269,8 @@ def erosion(pixel_array, image_width, image_height):
     for i in range(1, image_height - 1):
         for j in range(1, image_width - 1):
 
+            # Fit flag, assume true, flag false if we find one
+            # neighbouring pixel that does not match se if se = 1
             fit = True
 
             for k in [-1, 0, 1]:
@@ -251,29 +287,41 @@ def erosion(pixel_array, image_width, image_height):
     return image
 
 
-def connectedComponent(pixel_array, image_width, image_height):
+def computeConnectedComponent(pixel_array, image_width, image_height):
+    '''
+    This function computes and finds the connected components within the pixel_array.
+    Each connected component is assigned a label starting from 1 onwards. This uses a
+    4-neighbourhood scheme to determine 'connectedness'. This function follows the standard
+    connected component algorithm.
+    '''
+
     image = createInitializedGreyscalePixelArray(image_width, image_height)
+
+    # This array keeps track of the pixels that have been visited (1) or not visited (0).
     visited = createInitializedGreyscalePixelArray(image_width, image_height)
 
     label = 1
     info = {}
 
+    # Iterate over each pixel
     for i in range(image_height):
         for j in range(image_width):
 
+            # If the pixel is 'active' and has not been visited
             if pixel_array[i][j] != 0 and visited[i][j] != 1:
 
-                q = Queue()
-                q.enqueue((i, j))
-                visited[i][j] = 1
-                count = 0
+                q = Queue()          # Initialize new queue
+                q.enqueue((i, j))    # Enqueue the pixel coordinates
+                visited[i][j] = 1    # Update visited array
+                count = 0            # Initialize a count for the number of pixel within this connected component
 
                 while not q.isEmpty():
 
-                    (y, x) = q.dequeue()
-                    image[y][x] = label
-                    count += 1
+                    (y, x) = q.dequeue()    # Dequeue pixel
+                    image[y][x] = label     # Label the image with current connected component label
+                    count += 1              # Increment count
 
+                    # Denotes the 4-neighbourhood region to check around
                     neighbours = [
                         (y + 1, x),
                         (y - 1, x),
@@ -285,6 +333,7 @@ def connectedComponent(pixel_array, image_width, image_height):
 
                         if -1 < px[0] < image_height and -1 < px[1] < image_width:
 
+                            # If the pixel is 'active' and has not been visited
                             if pixel_array[px[0]][px[1]] != 0 and visited[px[0]][px[1]] != 1:
                                 q.enqueue((px[0], px[1]))
                                 visited[px[0]][px[1]] = 1
@@ -293,6 +342,51 @@ def connectedComponent(pixel_array, image_width, image_height):
                 label += 1
 
     return image, info
+
+
+def computeBoundingBox(pixel_array, image_width, image_height, cc_dict):
+    '''
+    This function computes the bounding box region for the license plate.
+    '''
+
+    aspect_ratio = 0
+    cc_index = -1
+
+    # Keep iterating through each connected component until it meets the following
+    # requirements.
+    while not (1.5 < aspect_ratio < 5) and cc_index < len(cc_dict) - 1:
+
+        cc_index += 1
+        label = cc_dict[cc_index][0]
+
+        min_x = image_width - 1
+        max_x = 0
+        min_y = image_height - 1
+        max_y = 0
+
+        for i in range(image_height):
+            for j in range(image_width):
+
+                if pixel_array[i][j] == label:
+
+                    if j < min_x:
+                        min_x = j
+
+                    if j > max_x:
+                        max_x = j
+
+                    if i < min_y:
+                        min_y = i
+
+                    if i > max_y:
+                        max_y = i
+
+        # Avoid division by zero
+        if max_y != min_y:
+            aspect_ratio = (max_x - min_x) / (max_y - min_y)
+
+    return min_x, max_x, min_y, max_y
+
 
 
 # This is our code skeleton that performs the license plate detection.
@@ -305,7 +399,7 @@ def main():
     SHOW_DEBUG_FIGURES = True
 
     # this is the default input image filename
-    input_filename = "numberplate4.png"
+    input_filename = "numberplate2.png"
 
     if command_line_arguments != []:
         input_filename = command_line_arguments[0]
@@ -326,103 +420,68 @@ def main():
     (image_width, image_height, px_array_r, px_array_g, px_array_b) = readRGBImageToSeparatePixelArrays(input_filename)
 
     # setup the plots for intermediate results in a figure
-    fig1, axs1 = pyplot.subplots(7, 1)
-    # axs1[0, 0].set_title('Input red channel of image')
-    # axs1[0, 0].imshow(px_array_r, cmap='gray')
-    # axs1[0, 1].set_title('Input green channel of image')
-    # axs1[0, 1].imshow(px_array_g, cmap='gray')
-    # axs1[1, 0].set_title('Input blue channel of image')
-    # axs1[1, 0].imshow(px_array_b, cmap='gray')
-
-    fig1.set_figwidth(5)
-    fig1.set_figheight(21)
+    fig1, axs1 = pyplot.subplots(4, 2)
+    fig1.set_figwidth(15)
+    fig1.set_figheight(20)
 
 
-    # STUDENT IMPLEMENTATION here
+    # STUDENT IMPLEMENTATION
 
     # Convert RGB image to Greyscale
     grey_array = convertToGreyscale(image_width, image_height, px_array_r, px_array_g, px_array_b)
-    axs1[0].set_title('Greyscale')
-    axs1[0].imshow(grey_array, cmap='gray')
+    axs1[0, 0].set_title('Greyscale')
+    axs1[0, 0].imshow(grey_array, cmap='gray')
 
     # Standard Deviation and Scale
-    px_array = standardDeviation(grey_array, image_width, image_height)
-    px_array = scale(px_array, image_width, image_height)
-    axs1[1].set_title('Standard Deviation and Scale')
-    axs1[1].imshow(px_array, cmap='gray')
+    px_array = computeStandardDeviation(grey_array, image_width, image_height)
+    axs1[0, 1].set_title('Standard Deviation')
+    axs1[0, 1].imshow(px_array, cmap='gray')
+
+    # Scale Intensities
+    px_array = computeScale(px_array, image_width, image_height)
+    axs1[1, 0].set_title('Scale')
+    axs1[1, 0].imshow(px_array, cmap='gray')
 
     # Threshold Segmentation
-    px_array = thresholdSegmentation(px_array, image_width, image_height, 140)
-    axs1[2].set_title('Threshold Segmentation')
-    axs1[2].imshow(px_array, cmap='g    ray')
+    px_array = computeThresholdSegmentation(px_array, image_width, image_height, 140)
+    axs1[1, 1].set_title('Threshold Segmentation')
+    axs1[1, 1].imshow(px_array, cmap='gray')
 
     # Dilation operations
-    for i in range(5):
-        px_array = dilation(px_array, image_width, image_height)
+    for i in range(3):
+        px_array = computeDilation(px_array, image_width, image_height)
 
-    axs1[3].set_title('Dilation Operations')
-    axs1[3].imshow(px_array, cmap='gray')
+    axs1[2, 0].set_title('Dilation Operations')
+    axs1[2, 0].imshow(px_array, cmap='gray')
 
     # Erosion operations
-    for i in range(5):
-        px_array = erosion(px_array, image_width, image_height)
+    for i in range(1):
+        px_array = computeErosion(px_array, image_width, image_height)
 
-    axs1[4].set_title('Erosion Operations')
-    axs1[4].imshow(px_array, cmap='gray')
-
-    # Open operation
-    px_array = erosion(px_array, image_width, image_height)
-    px_array = dilation(px_array, image_width, image_height)
+    axs1[2, 1].set_title('Erosion Operations')
+    axs1[2, 1].imshow(px_array, cmap='gray')
 
     # Connected Component Analysis
-    px_array, cc_dict = connectedComponent(px_array, image_width, image_height)
-    axs1[5].set_title('Connected Component Detection')
-    axs1[5].imshow(px_array, cmap='gray')
+    px_array, cc_dict = computeConnectedComponent(px_array, image_width, image_height)
+    axs1[3, 0].set_title('Connected Component Detection')
+    axs1[3, 0].imshow(px_array, cmap='gray')
 
     # Sort to find largest connect component
     cc_dict = sorted(cc_dict.items(), key=lambda item: item[1], reverse=True)
 
     # Compute bounding box
-    aspect_ratio = 0
-    cc_index = -1
-
-    while not (1.5 < aspect_ratio < 5):
-        cc_index += 1
-        label = cc_dict[cc_index][0]
-        bbox_min_x = image_width - 1
-        bbox_max_x = 0
-        bbox_min_y = image_height - 1
-        bbox_max_y = 0
-
-        for i in range(image_height):
-            for j in range(image_width):
-
-                if px_array[i][j] == label:
-
-                    if j < bbox_min_x:
-                        bbox_min_x = j
-
-                    if j > bbox_max_x:
-                        bbox_max_x = j
-
-                    if i < bbox_min_y:
-                        bbox_min_y = i
-
-                    if i > bbox_max_y:
-                        bbox_max_y = i
-
-        aspect_ratio = (bbox_max_x - bbox_min_x) / (bbox_max_y - bbox_min_y)
+    bbox_min_x, bbox_max_x, bbox_min_y, bbox_max_y = computeBoundingBox(px_array, image_width, image_height, cc_dict)
 
 
     # Draw a bounding box as a rectangle into the input image
-    axs1[6].set_title('Final image of detection')
-    axs1[6].imshow(grey_array, cmap='gray')
+    axs1[3, 1].set_title('Final image of detection')
+    axs1[3, 1].imshow(grey_array, cmap='gray')
     rect = Rectangle((bbox_min_x, bbox_min_y), bbox_max_x - bbox_min_x, bbox_max_y - bbox_min_y, linewidth=1,
                      edgecolor='g', facecolor='none')
-    axs1[6].add_patch(rect)
+    axs1[3, 1].add_patch(rect)
 
     # write the output image into output_filename, using the matplotlib savefig method
-    extent = axs1[6].get_window_extent().transformed(fig1.dpi_scale_trans.inverted())
+    extent = axs1[3, 1].get_window_extent().transformed(fig1.dpi_scale_trans.inverted())
     pyplot.savefig(output_filename, bbox_inches=extent, dpi=600)
 
     if SHOW_DEBUG_FIGURES:
